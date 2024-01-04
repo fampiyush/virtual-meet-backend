38ce585cb98a22dd85f7dc35898c48ff577d5e0f
Piyush Gupta <piyushgupta941d@gmail.com>
2024-01-05 00:18:53 +0530

    testing transfer by only poition

--- index.js
+++ index.js
@@ -14,10 +14,20 @@ app.get('/', (req, res) => {
   res.send('Welcome to socket io of virtual meet');
 });
 
+const users = {};
+
 io.on('connection', (socket) => {
     console.log('a user connected with id:', socket.id);
+    socket.on('user-model', (data) => {
+      // console.log(data);
+      users[socket.id] = data;
+      socket.broadcast.emit('user-model',{id: socket.id, position: data});
+    });
+
+    socket.emit('get-all-users', Object.values(users));
     socket.on('disconnect', () => {
       console.log('a user disconnected with id:', socket.id);
+      delete users[socket.id];
     });
 });
 
