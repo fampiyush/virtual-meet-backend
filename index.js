6489bac4c7e4736abe6128473349a95b61822707
Piyush Gupta <piyushgupta941d@gmail.com>
2024-01-17 23:13:02 +0530

    rooms implemented

--- index.js
+++ index.js
@@ -17,17 +17,48 @@ app.get('/', (req, res) => {
 });
 
 const users = {};
+const rooms = io.sockets.adapter.rooms;
 
 io.on('connection', (socket) => {
+  let currRoom;
     console.log('a user connected with id:', socket.id);
+    socket.on('join', (room) => {
+      const roomValues = [...rooms].map(([roo]) => roo);
+      if(room){
+        if(roomValues.includes(room)){
+          socket.join(room);
+          socket.emit('joined-room', room)
+          console.log(socket.id, 'joined room:', room)
+        }else {
+          socket.emit('joined-room', false)
+          socket.disconnect()
+        }
+      }else{
+        let roomId = generateId()
+        while (roomValues.includes(roomId)) {
+          roomId = generateId()
+        }
+        socket.join(roomId)
+        socket.emit('joined-room', roomId)
+        console.log(socket.id, 'created room:', roomId)
+      }
+    })
+   
     socket.on('user-model', (data) => {
+      currRoom = [...socket.rooms];
       users[socket.id] = data;
-      socket.broadcast.emit('user-model',{id: socket.id, data: data});
+      socket.broadcast.to(currRoom[1]).emit('user-model', { id: socket.id, data: data });
     });
-
+    
     socket.on('get-all-users', () => {
+      currRoom = [...socket.rooms]
       const allUsers = { ...users }
       delete allUsers[socket.id];
+      Object.keys(allUsers).forEach(key => {
+        if (allUsers[key]['room'] !== currRoom[1]) {
+          delete allUsers[key]
+        }
+      })
       socket.emit('all-users', allUsers);
     });
     
@@ -35,7 +66,7 @@ io.on('connection', (socket) => {
       console.log('a user disconnected with id:', socket.id);
       const data = users[socket.id];
       if (data) {
-        socket.broadcast.emit('user-disconnected', {socketId: socket.id, peerId: data['peerId']});
+        socket.broadcast.to(currRoom[1]).emit('user-disconnected', {socketId: socket.id, peerId: data['peerId']});
         delete users[socket.id];
       }
     });
@@ -44,3 +75,12 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`server running at port ${port}`);
 });
+
+const generateId = () => {
+   const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+   let randomCode = '';
+   for (let i = 0; i < 8; i++) {
+     randomCode += characters.charAt(Math.floor(Math.random() * characters.length));
+   }
+    return randomCode;
+}
